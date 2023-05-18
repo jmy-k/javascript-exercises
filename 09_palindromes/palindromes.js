6ee890a27c433df11622cb1a8b1683badd9eb01e
jmy-k <myjuliakan@gmail.com>
2023-05-18 14:28:37 -0400

    completed! with a lot of help... need to look closely at regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const checkedString = string.toLowerCase().replace(/^[a-z]/g,"");
+    return checkedString.split("").reverse().join("") === checkedString;
 };
 
 // Do not edit below this line
